b9af123d0928e784170881a10cb4fb9a438d7467
Husseini <mhmd.husseinyy@gmail.com>
2023-07-23 23:02:24 +0300

    create join and create classes dropdown list

--- js/toggle-sidebar.js
+++ js/toggle-sidebar.js
@@ -17,3 +17,18 @@ window.addEventListener("click", (event) => {
     sliderOpen.classList.remove("show-slider");
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const plusIcon = document.querySelector(".plus-icon");
+  const dropdown = document.getElementById("dropdown");
+
+  plusIcon.addEventListener("click", function () {
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    dropdown.classList.toggle('show');
+
+  });
+});
+
+
+
