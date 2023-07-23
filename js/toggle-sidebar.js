0c11e3389b75152da5442668d34506b22cf711a0
HaidarMestrah <10120319@mu.edu.lb>
2023-07-23 05:16:29 +0300

    Code Formatting

--- js/toggle-sidebar.js
+++ js/toggle-sidebar.js
@@ -1,16 +1,19 @@
-const hamburgerIconCont = document.getElementById('hamburger-icon-cont');
-const sliderOpen = document.querySelector('.slider-open');
+const hamburgerIconCont = document.getElementById("hamburger-icon-cont");
+const sliderOpen = document.querySelector(".slider-open");
 
-hamburgerIconCont.addEventListener('click', () => {
-  sliderOpen.classList.toggle('show-slider');
+hamburgerIconCont.addEventListener("click", () => {
+  sliderOpen.classList.toggle("show-slider");
 });
 
 function isClickInsideSlider(event) {
-  return sliderOpen.contains(event.target) || hamburgerIconCont.contains(event.target);
+  return (
+    sliderOpen.contains(event.target) ||
+    hamburgerIconCont.contains(event.target)
+  );
 }
 
-window.addEventListener('click', (event) => {
+window.addEventListener("click", (event) => {
   if (!isClickInsideSlider(event)) {
-    sliderOpen.classList.remove('show-slider');
+    sliderOpen.classList.remove("show-slider");
   }
 });
