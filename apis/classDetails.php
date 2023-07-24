a4cecffd91276d9ab201d4fae332c5b7eeaa7fbf
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-07-24 22:57:13 +0300

    Updating the ClassDetails API

--- apis/classDetails.php
+++ apis/classDetails.php
@@ -1,21 +1,31 @@
 <?php
 include('connection.php');
 
-$classId = $_GET['class_id'];
+$classId = $_POST['class_id'];
 
-// $stmt = $mysqli->prepare("SELECT class_name, classe_description, is_teacher,  .........
-//                          FROM classes 
-//                          JOIN user_classes ON classes.class_id = user_classes.class_id
-//                          WHERE class_id = ?");
-// $stmt->bind_param("i", $classId);
-// $stmt->execute();
-// $result = $stmt->get_result();
+$query = $mysqli->prepare("SELECT *
+                          FROM classes cls, posts pst, assignments asgn, user_classes usr_cls
+                          WHERE cls.class_id = ? AND
+	                        cls.class_id = asgn.assignment_class AND
+                            pst.post_class = cls.class_id AND
+                            cls.class_id = usr_cls.class_id;"
+                         );
+$query->bind_param("i", $classId);
+$query->execute();
+$result = $query->get_result();
+$class_exists = $query->num_rows();
 
-// $classes = array();
-// while ($row = $result->fetch_assoc()) {
-//     $classes[] = $row;
-// }
+if ($class_exists == 0) {
+    $classes = array();
+    while ($row = $result->fetch_assoc()) {
+        $classes[] = $row;
+    }
+	header('Content-Type: application/json');
+	echo json_encode($classes);
+} else{
+    $response['status'] = "failed";
+    $response['message'] = "This class ID doesn't exist";
+    echo json_encode($response);
+}
 
-// header('Content-Type: application/json');
-// echo json_encode($classes);
-// ?>
+?>
