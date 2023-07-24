d20f689718a73c97ef65a97a2f8228b1991471b5
Husseini <mhmd.husseinyy@gmail.com>
2023-07-24 14:34:42 +0300

    modifying createclass api

--- apis/createClass.php
+++ apis/createClass.php
@@ -4,9 +4,12 @@ include('connection.php');
 $classname = $_POST['class_name'];
 $classDescription = $_POST['class_description'];
 $classLink = $_POST['class_link'];
+$userId = $_POST['user_id'];
 
-$check_class = $mysqli->prepare('SELECT class_name FROM classes WHERE class_name=?');
-$check_class->bind_param('s', $classname);
+$check_class = $mysqli->prepare('SELECT c.class_name FROM classes c 
+                                INNER JOIN user_classes uc ON c.class_id = uc.class_id 
+                                WHERE c.class_name = ? AND uc.user_id = ?');
+$check_class->bind_param('si', $classname, $userId);
 $check_class->execute();
 $check_class->store_result();
 $class_exists = $check_class->num_rows();
@@ -16,17 +19,22 @@ if ($class_exists == 0) {
         $response['status'] = "failed";
         $response['message'] = "Class Name Can't be empty";
     } else {
-        $query = $mysqli->prepare('INSERT INTO classes(class_name, classe_description, class_link) VALUES(?,?,?)');
-        $query->bind_param('sss', $classname, $classDescription, $classLink);
-        $query->execute();
+        $queryInsertClass = $mysqli->prepare('INSERT INTO classes (class_name, classe_description, class_link) VALUES (?, ?, ?)');
+        $queryInsertClass->bind_param('sss', $classname, $classDescription, $classLink);
+        $queryInsertClass->execute();
+
+        $classId = $mysqli->insert_id;
+
+        $queryInsertUserClass = $mysqli->prepare('INSERT INTO user_classes (user_id, class_id, is_teacher) VALUES (?, ?, 1)');
+        $queryInsertUserClass->bind_param('ii', $userId, $classId);
+        $queryInsertUserClass->execute();
 
         $response['status'] = "success";
         $response['message'] = "Class added";
     }
 } else {
     $response['status'] = "failed";
-    $response['message'] = "class already exists";
+    $response['message'] = "Class already exists";
 }
 
 echo json_encode($response);
-
