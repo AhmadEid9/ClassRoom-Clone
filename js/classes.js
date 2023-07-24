67909b5dd5e4a8414e678fba5343b93f45828285
Husseini <mhmd.husseinyy@gmail.com>
2023-07-24 19:55:05 +0300

    fetching showing classes api in classes.js

--- js/classes.js
+++ js/classes.js
@@ -142,3 +142,68 @@ function createNewClass() {
     });
 }
 document.getElementById("submitClass").addEventListener("click", createNewClass);
+
+
+// show classes
+
+function fetchClassData() {
+  const user_id = localStorage.getItem("user_id");
+
+  axios
+    .get(`http://localhost/ClassRoom-Clone/apis/showclasses.php?user_id=${user_id}`)
+    .then(response => {
+      const classesData = response.data;
+      const classCardsContainer = document.querySelector(".class-cards-cont");
+
+      classesData.forEach(classData => {
+        const classCard = document.createElement("div");
+        classCard.classList.add("card");
+
+        classCard.innerHTML = `
+          <div class="card-top">
+            <div class="card-top-top">
+              <img src="https://lh3.googleusercontent.com/a-/AOh14Gh-UFCjdhVNr0xUwyC8QYyyZAnO_D-depcTebpu=s75-c" alt="" class="absolute" />
+              <a href="/dist/gym-class.html" style="text-decoration: none; color: #fff">
+                <div class="card-title"><h1>${classData.class_name}</h1></div>
+              </a>
+              <div class="card-title-classes"><h2>${classData.classe_description}</h2></div>
+            </div>
+            <div class="card-top-bottom">
+            </div>
+          </div>
+          <div class="card-middle">
+            <div class="card-middle-cont">
+              <div class="date">
+                <h4>Due Friday</h4>
+              </div>
+              <div class="hw-desc">
+                <h5>11:00 PM -Charbel's Assignment</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-bottom">
+            <div class="card-bottom-icon-cont">
+              <a href="#" class="link">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#606368">
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M19 5v14H5V5h14m0-2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 9c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm6 10H6v-1.53c0-2.5 3.97-3.58 6-3.58s6 1.08 6 3.58V18zm-9.69-2h7.38c-.69-.56-2.38-1.12-3.69-1.12s-3.01.56-3.69 1.12z" />
+                </svg>
+              </a>
+              <a href="#" class="link">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#606368">
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M9.17 6l2 2H20v10H4V6h5.17M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        `;
+
+        classCardsContainer.appendChild(classCard);
+      });
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+}
+fetchClassData();
