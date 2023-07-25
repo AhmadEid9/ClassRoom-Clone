23d0ee9a4ddf3deaf572133706f080b7255ea037
Husseini <mhmd.husseinyy@gmail.com>
2023-07-25 19:19:24 +0300

    fixing classDetails api

--- apis/classDetails.php
+++ apis/classDetails.php
@@ -2,15 +2,16 @@
 include('connection.php');
 
 $classId = $_POST['class_id'];
+$userId = $_POST['user_id'];
 
-$query = $mysqli->prepare("SELECT *
+$query = $mysqli->prepare("SELECT DISTINCT *
                             FROM classes cls
                             LEFT JOIN posts pst ON cls.class_id = pst.post_class
                             LEFT JOIN assignments asgn ON cls.class_id = asgn.assignment_class
                             LEFT JOIN user_classes usr_cls ON cls.class_id = usr_cls.class_id
-                            WHERE cls.class_id = ?;"
+                            WHERE cls.class_id = ? and usr_cls.user_id =? ;"
                          );
-$query->bind_param("i", $classId);
+$query->bind_param("ii", $classId, $userId);
 $query->execute();
 $result = $query->get_result();
 $class_exists = $query->num_rows();
@@ -27,5 +28,4 @@ if ($class_exists == 0) {
     $response['message'] = "This class ID doesn't exist";
     echo json_encode($response);
 }
-
 ?>
