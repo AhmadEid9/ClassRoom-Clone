bb411f5a0d51322ded416990fa2e8dc56f422e44
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2023-07-26 01:58:16 +0300

    removing password

--- apis/getClassPosts.php
+++ apis/getClassPosts.php
@@ -5,8 +5,9 @@ $class_id = $_POST['class_id'];
 $user_id = $_POST['user_id'];
 
 $stmt = $mysqli->prepare("SELECT posts.post_title, posts.post_description, user_name
-                         FROM posts, users, classes
+                         FROM posts, users, classes, user_classes
                          JOIN posts pts ON classes.class_id = pts.post_class
+                         JOIN user_classes usrs ON usrs.user_id = posts.post_teacher
                          WHERE posts.post_class = ? AND users.user_id = ?");
 $stmt->bind_param("ii",$class_id, $userId);
 $stmt->execute();
