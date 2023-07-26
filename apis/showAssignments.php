fc8787f8e0616f06aba236d34ffcdd8e564c08f7
Husseini <mhmd.husseinyy@gmail.com>
2023-07-26 04:13:18 +0300

    creating showAssignments api

---
+++ apis/showAssignments.php
@@ -0,0 +1,23 @@
+<?php
+include('connection.php');
+
+$classId = $_GET['class_id'];
+
+$stmt = $mysqli->prepare("SELECT a.assignment_title, a.assignment_description, a.assignment_due_date, u.user_name 
+                         FROM assignments a
+                         JOIN user_classes uc ON a.assignment_teacher = uc.user_id
+                         JOIN users u ON uc.user_id = u.user_id
+                         WHERE a.assignment_class = ?");
+
+$stmt->bind_param("i", $classId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$assign = array();
+while ($row = $result->fetch_assoc()) {
+    $assign[] = $row;
+}
+
+header('Content-Type: application/json');
+echo json_encode($assign);
+?>
